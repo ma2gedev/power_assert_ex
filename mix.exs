7024b982eb068112fc2ac574373a37d30f052882
Takayuki Matsubara <takayuki.1229@gmail.com>
2015-10-04 16:02:29 +0900

    bump version to 0.0.3

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule PowerAssert.Mixfile do
 
   def project do
     [app: :power_assert,
-     version: "0.0.2",
+     version: "0.0.3",
      elixir: "~> 1.0",
      description: "Power Assert in Elixir",
      package: [
