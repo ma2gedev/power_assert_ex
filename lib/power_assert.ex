f1194f81a0b12d9e56a89d34d869235385fc8658
Takayuki Matsubara <takayuki.1229@gmail.com>
2015-10-15 23:01:08 +0900

    add :use_ex_unit option for `use PowerAssert`
    Consider if other framework depending on ExUnit such as `ExSpec`,
    these framework imports `assert` macro so it's conflicted with our one.
    With this case, you set :use_ex_unit as true. PowerAssert module would
    import `power_assert` macro instead of `assert` macro. Also not
    `use ExUnit.Case` inside module.
    So you can use `power_assert` macro with your ExUnit based test
    framework.
    
    `:use_ex_unit` value is false as default
    
    related #1

--- lib/power_assert.ex
+++ lib/power_assert.ex
@@ -1,8 +1,20 @@
 defmodule PowerAssert do
   defmacro __using__(opts) do
-    quote do
-      use PowerAssert.ExUnitCase, unquote(opts)
-      import PowerAssert.Assertion
+    use_ex_unit = Keyword.get(opts, :use_ex_unit, false)
+    if !use_ex_unit do
+      quote do
+        use PowerAssert.ExUnitCase, unquote(opts)
+        import PowerAssert.Assertion
+      end
+    else
+      quote do
+        require PowerAssert.Assertion
+        defmacro power_assert(ast, msg \\ nil) do
+          quote do
+            PowerAssert.Assertion.assert(unquote(ast), unquote(msg))
+          end
+        end
+      end
     end
   end
 end
---
+++ test/power_assert_use_ex_unit_test.exs
@@ -0,0 +1,43 @@
+# when already `use ExUnit.Case`
+# for instance other framework depending on ExUnit such as `ExSpec`
+defmodule PowerAssertUseExUnitTest do
+  use ExUnit.Case
+  use PowerAssert, use_ex_unit: true
+
+  test "expr" do
+    import List
+    power_assert ~w(hoge fuga) == ["hoge", "fuga"]
+    x = "fuga"
+    power_assert "hoge#{x}fuga" == "hogefugafuga"
+    _one = "aiueo"
+    two = 2
+    power_assert [_one] = [two]
+    power_assert match?(x, "fuga")
+    keywords = [value: [value: "hoge"]]
+    power_assert keywords[:value][:value] == "hoge"
+    power_assert fn(x) -> x == 1 end.(1)
+    power_assert __ENV__.aliases |> Kernel.==([])
+    power_assert [1,2] |> first() |> Kernel.==(1)
+    power_assert self |> Kernel.==(self)
+    power_assert [1,2,3] |> Enum.take(1) |> List.delete(1) |> Enum.empty?
+  end
+
+  test "raise" do
+    try do
+      power_assert [1,2,3] |> Enum.take(1) |> Enum.empty?
+      assert false, "should not reach"
+    rescue
+      error ->
+        msg = """
+        [1, 2, 3] |> Enum.take(1) |> Enum.empty?()
+                          |               |
+                          [1]             false
+        """
+
+        if error.message <> "\n" != msg do
+          value = false
+          assert value
+        end
+    end
+  end
+end
