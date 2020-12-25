75986c3719c33bfc62b9e0f414bf50e45187da60
Takayuki Matsubara <takayuki.1229@gmail.com>
2020-12-25 23:26:07 +0900

    s/applications/extra_applications/
    - follow the CHANGELOG https://github.com/elixir-lang/elixir/blob/v1.4/CHANGELOG.md

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule PowerAssert.Mixfile do
   #
   # Type `mix help compile.app` for more information
   def application do
-    [applications: [:logger]]
+    [extra_applications: [:logger]]
   end
 
   # Dependencies can be Hex packages:
