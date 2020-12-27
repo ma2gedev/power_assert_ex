48f4b0e55b82ac6c0283cba1ac91f344a2985344
Takayuki Matsubara <takayuki.1229@gmail.com>
2020-12-27 23:43:52 +0900

    more concrete name

--- lib/power_assert/renderer.ex
+++ lib/power_assert/renderer.ex
@@ -4,19 +4,19 @@ defmodule PowerAssert.Renderer do
   """
 
   @doc false
-  def render_values(code_ast, values, left \\ nil, right \\ nil)
+  def render_values(code_ast, values, lhs_result \\ nil, rhs_result \\ nil)
 
-  def render_values(code_ast, [], left, right) do
-    Macro.to_string(code_ast) <> extra_information(left, right)
+  def render_values(code_ast, [], lhs_result, rhs_result) do
+    Macro.to_string(code_ast) <> extra_information(lhs_result, rhs_result)
   end
 
-  def render_values(code_ast, values, left, right) do
+  def render_values(code_ast, values, lhs_result, rhs_result) do
     code_str = Macro.to_string(code_ast)
     values = Enum.sort(values, fn [x_pos, _], [y_pos, _] -> x_pos > y_pos end)
     [max_pos, _] = Enum.max_by(values, fn [pos, _] -> pos end)
     first_line = String.duplicate(" ", max_pos + 1) |> replace_with_bar(values)
     lines = make_lines([], Enum.count(values), values, -1)
-    Enum.join([code_str, first_line] ++ lines, "\n") <> extra_information(left, right)
+    Enum.join([code_str, first_line] ++ lines, "\n") <> extra_information(lhs_result, rhs_result)
   end
 
   defp make_lines(lines, 0, _, _latest_pos) do
@@ -52,19 +52,19 @@ defmodule PowerAssert.Renderer do
     end)
   end
 
-  defp extra_information(left, right) when is_list(left) and is_list(right) do
+  defp extra_information(lhs_result, rhs_result) when is_list(lhs_result) and is_list(rhs_result) do
     [
-      "\n\nonly in lhs: " <> ((left -- right) |> inspect),
-      "only in rhs: " <> ((right -- left) |> inspect)
+      "\n\nonly in lhs: " <> ((lhs_result -- rhs_result) |> inspect),
+      "only in rhs: " <> ((rhs_result -- lhs_result) |> inspect)
     ]
     |> Enum.join("\n")
   end
 
-  defp extra_information(left, right) when is_map(left) and is_map(right) do
-    left = Map.delete(left, :__struct__)
-    right = Map.delete(right, :__struct__)
-    in_left = Map.split(left, Map.keys(right)) |> elem(1)
-    in_right = Map.split(right, Map.keys(left)) |> elem(1)
+  defp extra_information(lhs_result, rhs_result) when is_map(lhs_result) and is_map(rhs_result) do
+    lhs_result = Map.delete(lhs_result, :__struct__)
+    rhs_result = Map.delete(rhs_result, :__struct__)
+    in_left = Map.split(lhs_result, Map.keys(rhs_result)) |> elem(1)
+    in_right = Map.split(rhs_result, Map.keys(lhs_result)) |> elem(1)
     str = "\n"
 
     str =
@@ -81,7 +81,7 @@ defmodule PowerAssert.Renderer do
         str
       end
 
-    diff = collect_map_diff(left, right)
+    diff = collect_map_diff(lhs_result, rhs_result)
 
     str =
       case Enum.empty?(diff) do
@@ -92,16 +92,16 @@ defmodule PowerAssert.Renderer do
     str
   end
 
-  defp extra_information(left, right) do
-    if String.valid?(left) && String.valid?(right) do
-      extra_information_for_string(left, right)
+  defp extra_information(lhs_result, rhs_result) do
+    if String.valid?(lhs_result) && String.valid?(rhs_result) do
+      extra_information_for_string(lhs_result, rhs_result)
     else
       ""
     end
   end
 
-  defp extra_information_for_string(left, right) do
-    "\n\ndifference:" <> "\n" <> left <> "\n" <> right
+  defp extra_information_for_string(lhs_result, rhs_result) do
+    "\n\ndifference:" <> "\n" <> lhs_result <> "\n" <> rhs_result
   end
 
   defp collect_map_diff(map1, map2) do
